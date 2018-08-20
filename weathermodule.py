3d57cfc68b519f7e16fe06a022a710c1b4c45b3e
Ramsha Bukhari <ramsha.bukhari@arbisoft.com>
2018-08-20 14:24:59 +0500

    Add files via upload
    -changed the names of variables to meaningful names
    -replaced global variables and made them local
    -added methods docstrings

--- weathermodule.py
+++ weathermodule.py
@@ -2,20 +2,17 @@ import os
 import glob
 import csv
 import json
-from collections import defaultdict
-from collections import OrderedDict
 import argparse
 import datetime
-aDict = OrderedDict()
+
+from collections import defaultdict
+from collections import OrderedDict
+
+all_data_dict = OrderedDict()
 filtered_dict = OrderedDict()
 
-i = 0
-j = 0
-x = []
-y = []
 
-
-class LoadingData:
+class Data:
 
     def __init__(self, file_path):
         self.file_path = file_path
@@ -28,25 +25,22 @@ class LoadingData:
                     for row in csvReader:
                         temp = OrderedDict(zip(fields, row))
                         key = temp.pop(list(temp.keys())[0])
-                        aDict[key] = temp
+                        all_data_dict[key] = temp
 
 
-class ResultCalculation:
-
-    #def __init__(self, year, month):
-    #    self.year = year
-    #    self.month = month
+class Result:
 
     def yearly_data_calculation(self, year):
-        self.year=year
-        self.Max_Temp_Dict = ()
-        self.Min_Temp_Dict = ()
-        self.Max_Humidity_Dict = ()
-        self.Max_Temp_keys = ()
-        self.Min_Temp_keys = ()
-        self.Max_Humidity_keys = ()
+        """A function for filtering out the data of given year from the whole data and maximum temp, minimum temp and max humidity and their respective dates"""
+        self.year = year
+        Max_Temp = 0
+        Min_Temp = 0
+        Max_Humidity = 0
+        Max_Temp_key = 0
+        Min_Temp_key = 0
+        Max_Humidity_key = 0
 
-        for k, v in aDict.items():
+        for k, v in all_data_dict.items():
             if isinstance(v, OrderedDict):
                 for key, value in list(v.items()):
                     try:
@@ -55,25 +49,34 @@ class ResultCalculation:
                     except:
                         pass
 
-        for k in aDict.keys():
+        for k in all_data_dict.keys():
             if str(year) in k:
-                filtered_dict[k] = aDict.get(k)
+                filtered_dict[k] = all_data_dict.get(k)
 
-        Max_Temp_Dict = OrderedDict(sorted(filtered_dict.items(), key=lambda x: x[1]['Max TemperatureC'], reverse=True))
-        Min_Temp_Dict = OrderedDict(sorted(filtered_dict.items(), key=lambda x: x[1]['Min TemperatureC'], reverse=False))
-        Max_Humidity_Dict = OrderedDict(sorted(filtered_dict.items(), key=lambda x: x[1]['Max Humidity'], reverse=True))
-        Max_Temp_keys = list(Max_Temp_Dict.keys())
-        Min_Temp_keys = list(Min_Temp_Dict.keys())
-        Max_Humidity_keys = list(Max_Humidity_Dict.keys())
-        print('Maximum temperature value ' + str((Max_Temp_Dict.get(Max_Temp_keys[0])).get('Max TemperatureC')) + ' was recorded on ' + str(Max_Temp_keys[0]))
-        print('Minimum temperature value ' + str((Min_Temp_Dict.get(Min_Temp_keys[0])).get('Min TemperatureC')) + ' was recorded on ' + str(Min_Temp_keys[0]))
-        print('Maximum humidity value '  + str((Max_Humidity_Dict.get(Max_Humidity_keys[0])).get('Max Humidity')) + ' was recorded on ' + str(Max_Humidity_keys[0]))
+        for k, v in filtered_dict.items():
+            if isinstance(v, OrderedDict):
+                for key, value in list(v.items()):
+                    if key == 'Max TemperatureC':
+                        if int(v[key]) > int(Max_Temp):
+                            Max_Temp = v[key]
+                            Max_Temp_key = k
+                    if key == 'Min TemperatureC':
+                        if int(v[key]) < int(Min_Temp):
+                            Min_Temp = v[key]
+                            Min_Temp_key = k
+                    if key == 'Max Humidity':
+                        if int(v[key]) > int(Max_Humidity):
+                            Max_Humidity = v[key]
+                            Max_Humidity_key = k
 
-
+        print('Maximum temperature value ' + str(Max_Temp) + ' was recorded on ' + str(Max_Temp_key))
+        print('Minimum temperature value ' + str(Min_Temp) + ' was recorded on ' + str(Min_Temp_key))
+        print('Maximum humidity value ' + str(Max_Humidity) + ' was recorded on ' + str(Max_Humidity_key))
 
     def monthly_data_calculation(self, year, month):
-        self.year=year
-        self.month=month
+        """ A function for filtering out the data for given year and month and calculating averages of temperatures"""
+        self.year = year
+        self.month = month
 
         sum_highest_temp = 0
         sum_lowest_temp = 0
@@ -82,7 +85,7 @@ class ResultCalculation:
         average_lowest_temp = 0
         average_mean_humidity = 0
 
-        for k, v in aDict.items():
+        for k, v in all_data_dict.items():
             if isinstance(v, OrderedDict):
                 for key, value in list(v.items()):
                     try:
@@ -91,9 +94,9 @@ class ResultCalculation:
                     except:
                         pass
 
-        for k in aDict.keys():
+        for k in all_data_dict.keys():
             if str(year) + '-' + str(month) in k:
-                filtered_dict[k] = aDict.get(k)
+                filtered_dict[k] = all_data_dict.get(k)
 
         for k, v in filtered_dict.items():
             if isinstance(v, OrderedDict):
@@ -113,10 +116,15 @@ class ResultCalculation:
         print('Average Mean Humidity was:' + str(average_mean_humidity))
 
     def monthly_graph_plotting(self, year, month):
-        self.year=year
-        self.month=month
+        """A function for printing out the graphs in required format"""
+        self.year = year
+        self.month = month
+        list_max_temp = []
+        list_min_temp = []
+        i = 0
+        j = 0
 
-        for k, v in aDict.items():
+        for k, v in all_data_dict.items():
             if isinstance(v, OrderedDict):
                 for key, value in list(v.items()):
                     try:
@@ -125,64 +133,60 @@ class ResultCalculation:
                     except:
                         pass
 
-        for k in aDict.keys():
+        for k in all_data_dict.keys():
             if str(year) + '-' + str(month) in k:
-                filtered_dict[k] = aDict.get(k)
+                filtered_dict[k] = all_data_dict.get(k)
 
         for k, v in filtered_dict.items():
             if isinstance(v, OrderedDict):
                 for key, value in v.items():
                     if key == 'Max TemperatureC':
-                        x.append(int(v[key]))
+                        list_max_temp.append(int(v[key]))
                     if key == 'Min TemperatureC':
-                        y.append(int(v[key]))
+                        list_min_temp.append(int(v[key]))
 
         red = '\033[91m'
         blue = '\033[94m'
-        grey = '\033[37m'
-        global i
-        global j
-        while i < len(x):
-            print(i, red, '+' * x[i], x[i], 'C')
-            print(i, blue, '+' * y[i], y[i], 'C')
+
+        while i < len(list_max_temp):
+            print(i, red, '+' * list_max_temp[i], list_max_temp[i], 'C')
+            print(i, blue, '+' * list_min_temp[i], list_min_temp[i], 'C')
             i += 1
 
-        while j < len(x):
-            print(j, blue, '+' * y[j], red, '+' * x[j], y[j], 'C', '-', x[j], 'C')
+        while j < len(list_max_temp):
+            print(
+            j, blue, '+' * list_min_temp[j], red, '+' * list_max_temp[j], list_min_temp[j], 'C', '-', list_max_temp[j],
+            'C')
             j += 1
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help='Enter the path to directory containing your weather files')
-    parser.add_argument('-e','--date1',type=str)
+    parser.add_argument('-e', '--date1', type=str)
     parser.add_argument('-a', '--date2', type=str)
-    parser.add_argument('-c', '--date3',type=str)
+    parser.add_argument('-c', '--date3', type=str)
 
     args = parser.parse_args()
     file_path = args.path
-    load_data = LoadingData(file_path)
+    load_data = Data(file_path)
 
     if args.date2:
-        date2=args.date2
+        date2 = args.date2
         date2 = datetime.datetime.strptime(date2, "%Y/%m")
-        calcobj = ResultCalculation()
+        calcobj = Result()
         calcobj.monthly_data_calculation(date2.year, date2.month)
 
     if args.date3:
         date3 = args.date3
         date3 = datetime.datetime.strptime(date3, "%Y/%m")
-        print(date3.year)
-        print(date3.month)
-        calcobj = ResultCalculation()
+        calcobj = Result()
         calcobj.monthly_graph_plotting(date3.year, date3.month)
 
     if args.date1:
-        date1=args.date1
-        calcobj = ResultCalculation()
+        date1 = args.date1
+        calcobj = Result()
         calcobj.yearly_data_calculation(date1)
-
-
 
 
 if __name__ == '__main__':
